425d34133fdfe36813de4c6171ed04194a84221a
drakkomaximo <drakkomaximo1234@gmail.com>
2023-06-25 14:30:05 -0500

    local changes

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,2 @@
-export const API_URL = "https://crud-auth-v2--drakkomaximo.repl.co/api";
+/* export const API_URL = "https://crud-auth-v2--drakkomaximo.repl.co/api"; */
+export const API_URL = "http://localhost:4000/api";
